4399a10218a51d72b9a1345db6e6885ac7e91387
Arne <arnetraue@gmx.de>
2022-02-15 18:37:09 +0100

    Minor adaptions to GEM Update

--- example.py
+++ example.py
@@ -1,15 +1,16 @@
 import gym_electric_motor as gem
+from gym_electric_motor import reference_generators as rg
 import gem_controllers as gc
 from gym_electric_motor.visualization import MotorDashboard
 from gym_electric_motor.state_action_processors import FluxObserver
 
 
-env_id = 'AbcCont-TC-SCIM-v0'
+env_id = 'Cont-CC-SCIM-v0'
 
 env = gem.make(
         env_id,
         state_action_processors=(FluxObserver(),),
-        visualization=MotorDashboard(state_plots=['torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq', 'psi_abs']),
+        visualization=MotorDashboard(state_plots=['omega', 'torque', 'i_sd', 'i_sq', 'u_sd', 'u_sq', 'psi_angle', 'psi_abs']),
     )
 
 state, reference = env.reset()
@@ -20,6 +21,6 @@ c = gc.GemController.make(
     env_id,
     a=5,
     current_safety_margin=0.15,
-    )
+)
 
 c.control_environment(env, n_steps=50001, render_env=True)
