da6dcd063c03847344bc7a68b25286dc26c43955
Felix Book <felix.book98@gmail.com>
2022-08-29 13:23:25 +0200

    Add requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+
+AUTHORS = ['Felix Book', 'Max Schenke', 'Barnabas Haucke-Korber', 'Oliver Wallscheid',]
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='gem_control',
+      version='0.0.1',
+      description='Classic Controllers for the gym-electric-motor toolbox',
+      packages=setuptools.find_packages(),
+      install_requires=requirements,
+      python_requires='>=3.6',
+      author=', '.join(sorted(AUTHORS, key=lambda n: n.split()[-1].lower())),
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      url="https://github.com/upb-lea/gem-control",
+      )
