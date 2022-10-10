cc5ce321b965e2c7407605e845892523750b18c4
Felix Book <felix.book98@gmail.com>
2022-10-10 11:08:53 +0200

    update examples and add usage guide

--- gem_controllers/reference_plotter.py
+++ gem_controllers/reference_plotter.py
@@ -2,12 +2,21 @@ from gym_electric_motor.visualization.motor_dashboard import StatePlot
 
 
 class ReferencePlotter:
+    """This class adds the reference values of the subordinate stages to the stage plots of the GEM environment."""
     def __init__(self):
         self._referenced_plots = []
         self._referenced_states = []
         self._plot_references = None
 
     def tune(self, env, referenced_states, plot_references, **_):
+        """
+        Tune the reference plotter.
+        Args:
+            env(ElectricMotorEnvironment): The GEM-Environment that the controller shall be created for.
+            referenced_states(np.ndarray): Array of all referenced states.
+            plot_references(bool): Flag, if the references of the subordinate stages should be plotted.
+
+        """
         if plot_references:
             for visualization in env.visualizations:
                 for time_plot in visualization._time_plots:
@@ -20,5 +29,11 @@ class ReferencePlotter:
             plot._referenced = True
 
     def update_plots(self, references):
+        """
+        Update the state plots of the GEM environment
+        Args:
+            references(np.ndarray): Array of all reference values of the subordinate stages.
+        """
+
         for plot, state in zip(self._referenced_plots, self._referenced_states):
             plot._ref_data[plot.data_idx] = references[state]
