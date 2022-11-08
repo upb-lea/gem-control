20e23f51cd110d8068ba18cd9d0281584d63cea7
Felix Book <felix.book98@gmail.com>
2022-11-08 18:06:53 +0100

    add discrete output functions for three-phase motors

--- gem_controllers/stages/disc_output_stage.py
+++ gem_controllers/stages/disc_output_stage.py
@@ -16,7 +16,7 @@ class DiscOutputStage(Stage):
 
     @output_stage.setter
     def output_stage(self, value):
-        assert value in [self.to_b6_discrete, self.to_multi_discrete, self.to_discrete]
+        assert value in [self.to_b6_discrete, self.to_multi_discrete, self.to_discrete, self.to_finite_multi]
         self._output_stage = value
 
     def __init__(self):
@@ -37,11 +37,15 @@ class DiscOutputStage(Stage):
 
     @staticmethod
     def to_b6_discrete(multi_discrete_action):
-        raise NotImplementedError
+        return np.sum([a * 2 ** n for n, a in enumerate(multi_discrete_action[::-1])])
 
     @staticmethod
     def to_multi_discrete(multi_discrete_action):
         return multi_discrete_action
+
+    @staticmethod
+    def to_finite_multi(multi_discrete_action):
+        return np.array([DiscOutputStage.to_b6_discrete(multi_discrete_action[:3]), multi_discrete_action[3]])
 
     def to_action(self, _state, reference):
         conditions = [reference <= self.low_level, reference >= self.high_level]
@@ -61,7 +65,8 @@ class DiscOutputStage(Stage):
         self.low_level = -0.33 * (voltage_range[1] - voltage_range[0])
         self.high_level = 0.33 * (voltage_range[1] - voltage_range[0])
 
-        if type(env.action_space) == gym.spaces.MultiDiscrete:
+        if type(env.action_space) == gym.spaces.MultiDiscrete \
+                and type(env.physical_system.converter) != cv.FiniteMultiConverter:
             self.output_stage = DiscOutputStage.to_multi_discrete
             self.low_action = []
             self.idle_action = []
@@ -78,6 +83,15 @@ class DiscOutputStage(Stage):
             self.low_action, self.idle_action, self.high_action = self._get_actions(env.action_space.n)
         elif type(env.physical_system.converter) == cv.FiniteB6BridgeConverter:
             self.output_stage = DiscOutputStage.to_b6_discrete
+            self.low_level = self.high_level = 0
+            self.low_action = 0
+            self.high_action = 1
+        elif type(env.physical_system.converter) == cv.FiniteMultiConverter:
+            self.output_stage = DiscOutputStage.to_finite_multi
+            self.low_action = [0, 0, 0, 2]
+            self.high_action = [1, 1, 1, 1]
+            self.idle_action = [0, 0, 0, 0]
+
         else:
             raise Exception(f'No discrete output stage available for action space {env.action_space}.')
 
